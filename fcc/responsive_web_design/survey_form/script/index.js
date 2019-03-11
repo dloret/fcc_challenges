f575e9ba1886c6dd758bd3bc60050b97f1aa54ff
Donatien LORET <donatienloret@gmail.com>
2019-03-11 06:02:31 +0100

    JS - Implemented validation on all screens and disabled buttons as long as validation is ko

--- fcc/responsive_web_design/survey_form/script/index.js
+++ fcc/responsive_web_design/survey_form/script/index.js
@@ -15,3 +15,67 @@ buttons.forEach((button) => {
     sections[displayedSection].classList.remove('hide');
   }, true);
 });
+
+// eslint-disable-next-line spaced-comment
+/********************
+* Fields validation *
+*********************/
+buttons[displayedSection].setAttribute('disabled', true);
+
+function validateNotEmpty(trackedEvent, element) {
+  element.addEventListener(trackedEvent, (event) => {
+    if (!event.target.value) {
+      element.classList.add('validation-ko');
+      buttons[displayedSection].setAttribute('disabled', true);
+    } else {
+      element.classList.remove('validation-ko');
+      element.classList.add('validation-ok');
+      buttons[displayedSection].removeAttribute('disabled');
+    }
+  }, true);
+}
+
+function validateEmail(trackedEvent, element) {
+  const regex = /^[a-zA-Z0-9_'.-]+@[a-zA-Z0-9_.-]+\.[a-zA-Z]+$/;
+  element.addEventListener(trackedEvent, (event) => {
+    if (!regex.test(event.target.value)) {
+      element.classList.add('validation-ko');
+      buttons[displayedSection].setAttribute('disabled', true);
+    } else {
+      element.classList.remove('validation-ko');
+      element.classList.add('validation-ok');
+      buttons[displayedSection].removeAttribute('disabled');
+    }
+  }, true);
+}
+
+function validateGlobalScore(trackedEvent, element) {
+  const regex = /[0-9]{1,2}/;
+  element.addEventListener(trackedEvent, (event) => {
+    const note = event.target.value;
+    if (!note || !regex.test(note) || (note < 1 || note > 10)) {
+      element.classList.add('validation-ko');
+      buttons[displayedSection].setAttribute('disabled', true);
+    } else {
+      element.classList.remove('validation-ko');
+      element.classList.add('validation-ok');
+      buttons[displayedSection].removeAttribute('disabled');
+    }
+  }, true);
+}
+
+const firstName = document.getElementById('firstName');
+validateNotEmpty('change', firstName);
+validateNotEmpty('focusout', firstName);
+
+const lastName = document.getElementById('name');
+validateNotEmpty('change', lastName);
+validateNotEmpty('focusout', lastName);
+
+const email = document.getElementById('email');
+validateEmail('change', email);
+validateEmail('focusout', email);
+
+const gScore = document.getElementById('number');
+validateGlobalScore('change', gScore);
+validateGlobalScore('focusout', gScore);
