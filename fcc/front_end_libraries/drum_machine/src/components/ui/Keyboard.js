5dfdb3ed4d1f5b70cc3fe7ce65b8b0cd86cc5504
Donatien LORET <donatienloret@gmail.com>
2019-08-06 11:26:06 +0200

    Drum Machine: archiving the last tests to audio element in Keyboard

--- fcc/front_end_libraries/drum_machine/src/components/ui/Keyboard.js
+++ fcc/front_end_libraries/drum_machine/src/components/ui/Keyboard.js
@@ -1,21 +1,32 @@
 import React from 'react';
 
 import sounds from '../../sounds/sounds.json';
+
+import DR660_808_Clap from '../../sounds/DR660_808_Clap.wav';
+import DR660_808_High_Tom from '../../sounds/DR660_808_High_Tom.wav';
+import DR660_808_Snare from '../../sounds/DR660_808_Snare.wav';
 console.log(sounds);
+
+const handleClick = event => {
+  event.persist();
+  console.log(event.nativeEvent);
+  event.nativeEvent.srcElement.children[0].play();
+};
 
 export default function Keyboard() {
   return (
     <section>
-      {sounds.map(sound => {
-        import(`../../sounds/${sound.fileName}.${sound.ext}`)
-          .then(module => (
-            <button id={sound.key} className="drum-pad" key={sound.key}>
-              <audio className="clip" src={module} type="audio/wav" />
-              {sound.key}
-            </button>
-          ))
-          .catch(error => console.log(error));
-      })}
+      {sounds.map(sound => (
+        <button
+          id={sound.key}
+          className="drum-pad"
+          key={sound.key}
+          onClick={handleClick}
+        >
+          <audio className="clip" src={sound.fileName} type="audio/wav" />
+          {sound.key}
+        </button>
+      ))}
     </section>
   );
 }
